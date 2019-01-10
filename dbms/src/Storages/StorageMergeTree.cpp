aa6123badb3e1faad4eab239727cd1b054e3c96d
Alexey Zatelepin <ztlpn@yandex-team.ru>
2019-01-10 21:19:29 +0300

    KILL MUTATION skeleton

--- dbms/src/Storages/StorageMergeTree.cpp
+++ dbms/src/Storages/StorageMergeTree.cpp
@@ -348,7 +348,6 @@ void StorageMergeTree::mutate(const MutationCommands & commands, const Context &
     background_task_handle->wake();
 }
 
-
 std::vector<MergeTreeMutationStatus> StorageMergeTree::getMutationsStatus() const
 {
     std::lock_guard lock(currently_merging_mutex);
@@ -389,6 +388,11 @@ std::vector<MergeTreeMutationStatus> StorageMergeTree::getMutationsStatus() cons
     }
 
     return result;
+}
+
+void StorageMergeTree::killMutation(const String & mutation_id)
+{
+    LOG_TRACE(log, "KILL MUTATION " << mutation_id);
 }
 
 
